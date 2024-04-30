668884526da788fa21aedbb122d4e263f422a73d
Jared Landau <jaredlandau@gmail.com>
2024-05-01 00:30:38 +1000

    Refactored inference script
    inference.timesformer.py
    * Updated script to be (mostly) PEP 8 compliant
    * Changed some variable names to make them more readable
    * Changed console output to be clearer and more verbose (will update this to use 'logging' eventually)
    * Fixed whitespace inconsistencies
    * Fixed 'divide by zero error' in predict_fn when normalising mask_prediction
    * Running inference on files in the 'layers' folder of a segment will now work with any naming convention

--- inference_timesformer.py
+++ inference_timesformer.py
@@ -19,30 +19,36 @@ from torch.utils.data import DataLoader, Dataset
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+import glob
+import PIL.Image
 from warmup_scheduler import GradualWarmupScheduler
 from tqdm.auto import tqdm
-import PIL.Image
 from tap import Tap
-import glob
 
 PIL.Image.MAX_IMAGE_PIXELS = 933120000
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+
 class InferenceArgumentParser(Tap):
-    segment_id:list[str] = ['20231210121321']
-    segment_path:str = './scrolls'
-    model_path:str = './checkpoints/timesformer_wild15_20230702185753_0_fr_i3depoch=12.ckpt'
-    out_path:str = "./predictions"
-    stride:int = 2
-    start:int = 32
-    num_layers:int = 1
-    workers:int = 4
-    batch_size:int = 512
-    size:int = 64
-    reverse:int =  0
-    device:str ='cuda'
+    segment_id: list[str] = ['20231210121321']
+    segment_path: str = './scrolls'
+    model_path: str = './checkpoints/timesformer_wild15_20230702185753_0_fr_i3depoch=12.ckpt'
+    out_path: str = "./predictions"
+    stride: int = 2
+    start: int = 32
+    num_layers: int = 1
+    workers: int = 4
+    batch_size: int = 512
+    size: int = 64
+    reverse: int = 0
+    device: str = 'cuda'
     format = 'tif'
+
+
+# Parse arguments
 args = InferenceArgumentParser().parse_args()
+
+
 def gkern(kernlen=21, nsig=3):
     """Returns a 2D Gaussian kernel."""
     x = np.linspace(-nsig, nsig, kernlen+1)
@@ -50,37 +56,39 @@ def gkern(kernlen=21, nsig=3):
     kern2d = np.outer(kern1d, kern1d)
     return kern2d/kern2d.sum()
 
+
 class CFG:
-    # ============== comp exp name =============
+    # ============== File structure =============
     comp_name = 'vesuvius'
     comp_dir_path = './'
     comp_folder_name = './'
     comp_dataset_path = './'
     exp_name = 'pretraining_all'
 
-    # ============== model cfg =============
-    num_layers = args.num_layers # number of layers
+    # ==============  Model config  =============
+    num_layers = args.num_layers
     encoder_depth = 5
 
-    # ============== training cfg =============
+    # ==============  Training config  =============
     size = 64
     tile_size = 64
     stride = tile_size // 3
-    train_batch_size = 256 # 32
+    train_batch_size = 256
     valid_batch_size = 256
     use_amp = True
 
     scheduler = 'GradualWarmupSchedulerV2'
-    epochs = 10 # 30
+    epochs = 3
+    # Best results between 3-5 epochs, too many and you will start over-fitting
 
-    # adamW warmupあり
+    # AdamW warmup
     warmup_factor = 10
     lr = 1e-4 / warmup_factor
     min_lr = 1e-6
     num_workers = 16
     seed = 42
 
-    # ============== augmentation =============
+    # ============== Augmentation =============
     valid_aug_list = [
         A.Resize(size, size),
         A.Normalize(
@@ -89,6 +97,8 @@ class CFG:
         ),
         ToTensorV2(transpose_mask=True),
     ]
+
+
 def set_seed(seed=None, cudnn_deterministic=True):
     if seed is None:
         seed = 42
@@ -101,16 +111,16 @@ def set_seed(seed=None, cudnn_deterministic=True):
     torch.backends.cudnn.deterministic = cudnn_deterministic
     torch.backends.cudnn.benchmark = False
 
+
 def cfg_init(cfg, mode='val'):
     set_seed(cfg.seed)
-cfg_init(CFG)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def read_image_mask(fragment_id,start_idx,end_idx,rotation=0):
     images = []
-
     idxs = range(start_idx, end_idx)
+
+    print()
     for i in idxs:
         print(f"Loading image {(i+1) - start_idx} of {end_idx - start_idx}...")
         print(f"{args.segment_path}/{fragment_id}/layers/{i:02}.{args.format}")
@@ -118,7 +128,7 @@ def read_image_mask(fragment_id,start_idx,end_idx,rotation=0):
         pad0 = (256 - image.shape[0] % 256)
         pad1 = (256 - image.shape[1] % 256)
         image = np.pad(image, [(0, pad0), (0, pad1)], constant_values=0)
-        image=np.clip(image,0,200)
+        image = np.clip(image,0,200)
         images.append(image)
     print(f"Successfully loaded {end_idx - start_idx} images.")
 
@@ -126,12 +136,12 @@ def read_image_mask(fragment_id,start_idx,end_idx,rotation=0):
 
     if args.reverse != 0 or fragment_id in ['20230701020044','verso','20230901184804','20230901234823','20230531193658','20231007101615','20231005123333','20231011144857','20230522215721', '20230919113918', '20230625171244','20231022170900','20231012173610','20231016151000']:
         print("Reverse Segment")
-        images=images[:,:,::-1]
+        images = images[:,:,::-1]
     
-    fragment_mask=None
-    wildcard_path_mask = f'{args.segment_path}/{fragment_id}/*_mask.png'
-    if os.path.exists(f'{args.segment_path}/{fragment_id}/{fragment_id}_mask.png'):
-        fragment_mask=cv2.imread(CFG.comp_dataset_path + f"{args.segment_path}/{fragment_id}/{fragment_id}_mask.png", 0)
+    fragment_mask = None
+    wildcard_path_mask = f"{args.segment_path}/{fragment_id}/*_mask.png"
+    if os.path.exists(f"{args.segment_path}/{fragment_id}/{fragment_id}_mask.png"):
+        fragment_mask = cv2.imread(CFG.comp_dataset_path + f"{args.segment_path}/{fragment_id}/{fragment_id}_mask.png", 0)
         fragment_mask = np.pad(fragment_mask, [(0, pad0), (0, pad1)], constant_values=0)
     elif len(glob.glob(wildcard_path_mask)) > 0:
         # any *mask.png exists
@@ -142,19 +152,20 @@ def read_image_mask(fragment_id,start_idx,end_idx,rotation=0):
         # White mask
         fragment_mask = np.ones_like(images[:,:,0]) * 255
 
-    return images,fragment_mask
+    return images, fragment_mask
+
 
 def get_img_splits(fragment_id,start_idx,end_idx,rotation=0):
     images = []
     xyxys = []
-    image,fragment_mask = read_image_mask(fragment_id,start_idx,end_idx,rotation)
+    image, fragment_mask = read_image_mask(fragment_id,start_idx,end_idx,rotation)
     x1_list = list(range(0, image.shape[1]-CFG.tile_size+1, CFG.stride))
     y1_list = list(range(0, image.shape[0]-CFG.tile_size+1, CFG.stride))
     for y1 in y1_list:
         for x1 in x1_list:
             y2 = y1 + CFG.tile_size
             x2 = x1 + CFG.tile_size
-            if not np.any(fragment_mask[y1:y2, x1:x2]==0):
+            if not np.any(fragment_mask[y1:y2, x1:x2] == 0):
                 images.append(image[y1:y2, x1:x2])
                 xyxys.append([x1, y1, x2, y2])
     test_dataset = CustomDatasetTest(images,np.stack(xyxys), CFG,transform=A.Compose([
@@ -166,22 +177,27 @@ def get_img_splits(fragment_id,start_idx,end_idx,rotation=0):
         ToTensorV2(transpose_mask=True),
     ]))
 
-    test_loader = DataLoader(test_dataset,
-                              batch_size=CFG.valid_batch_size,
-                              shuffle=False,
-                              num_workers=CFG.num_workers, pin_memory=False, drop_last=False,
-                              )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size = CFG.valid_batch_size,
+        shuffle = False,
+        num_workers = CFG.num_workers, pin_memory=False, drop_last=False,
+    )
+
     return test_loader, np.stack(xyxys),(image.shape[0],image.shape[1]),fragment_mask
 
+
 def get_transforms(data, cfg):
+    aug = None
     if data == 'valid':
         aug = A.Compose(cfg.valid_aug_list)
     return aug
 
+
 class CustomDatasetTest(Dataset):
-    def __init__(self, images,xyxys, cfg, transform=None):
+    def __init__(self, images, xyxys, cfg, transform=None):
         self.images = images
-        self.xyxys=xyxys
+        self.xyxys = xyxys
         self.cfg = cfg
         self.transform = transform
 
@@ -190,18 +206,19 @@ class CustomDatasetTest(Dataset):
     
     def __getitem__(self, idx):
         image = self.images[idx]
-        xy=self.xyxys[idx]
+        xy = self.xyxys[idx]
         if self.transform:
             data = self.transform(image=image)
             image = data['image'].unsqueeze(0)
-        return image,xy
-    
+        return image, xy
+
+
 class RegressionPLModel(pl.LightningModule):
     def __init__(self,pred_shape,size=64,enc='',with_norm=False):
         super(RegressionPLModel, self).__init__()
         self.save_hyperparameters()
-        self.mask_pred = np.zeros(self.hparams.pred_shape)
-        self.mask_count = np.zeros(self.hparams.pred_shape)
+        self.mask_prediction = np.zeros(self.hparams.pred_shape)
+        self.mask_count = np.ones(self.hparams.pred_shape)
         self.loss_func1 = smp.losses.DiceLoss(mode='binary')
         self.loss_func2= smp.losses.SoftBCEWithLogitsLoss(smooth_factor=0.25)
         self.loss_func= lambda x,y:0.5 * self.loss_func1(x,y)+0.5*self.loss_func2(x,y)
@@ -222,12 +239,12 @@ class RegressionPLModel(pl.LightningModule):
             self.normalization=nn.BatchNorm3d(num_features=1)
 
     def forward(self, x):
-        if x.ndim==4:
-            x=x[:,None]
+        if x.ndim == 4:
+            x = x[:,None]
         if self.hparams.with_norm:
-            x=self.normalization(x)
+            x = self.normalization(x)
         x = self.backbone(torch.permute(x, (0, 2, 1,3,4)))
-        x=x.view(-1,1,4,4)        
+        x = x.view(-1,1,4,4)
         return x
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -239,24 +256,23 @@ class RegressionPLModel(pl.LightningModule):
         return {"loss": loss1}
 
     def validation_step(self, batch, batch_idx):
-        x,y,xyxys= batch
+        x,y,xyxys = batch
         batch_size = x.size(0)
         outputs = self(x)
         loss1 = self.loss_func(outputs, y)
         y_preds = torch.sigmoid(outputs).to('cpu')
         for i, (x1, y1, x2, y2) in enumerate(xyxys):
-            self.mask_pred[y1:y2, x1:x2] += F.interpolate(y_preds[i].unsqueeze(0).float(),scale_factor=16,mode='bilinear').squeeze(0).squeeze(0).numpy()
+            self.mask_prediction[y1:y2, x1:x2] += F.interpolate(y_preds[i].unsqueeze(0).float(),scale_factor=16,mode='bilinear').squeeze(0).squeeze(0).numpy()
             self.mask_count[y1:y2, x1:x2] += np.ones((self.hparams.size, self.hparams.size))
 
         self.log("val/MSE_loss", loss1.item(),on_step=True, on_epoch=True, prog_bar=True)
         return {"loss": loss1}
     
     def configure_optimizers(self):
-
         optimizer = AdamW(filter(lambda p: p.requires_grad, self.parameters()), lr=CFG.lr)
-    
         scheduler = get_scheduler(CFG, optimizer)
         return [optimizer],[scheduler]
+
 
 class GradualWarmupSchedulerV2(GradualWarmupScheduler):
     """
@@ -280,22 +296,26 @@ class GradualWarmupSchedulerV2(GradualWarmupScheduler):
         else:
             return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
 
+
 def get_scheduler(cfg, optimizer):
     scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, 10, eta_min=1e-6)
+        optimizer, 10, eta_min=1e-6
+    )
     scheduler = GradualWarmupSchedulerV2(
-        optimizer, multiplier=1.0, total_epoch=1, after_scheduler=scheduler_cosine)
-
+        optimizer, multiplier=1.0, total_epoch=1, after_scheduler=scheduler_cosine
+    )
     return scheduler
+
 
 def scheduler_step(scheduler, avg_val_loss, epoch):
     scheduler.step(epoch)
 
-def predict_fn(test_loader, model, device, test_xyxys,pred_shape):
-    mask_pred = np.zeros(pred_shape)
-    mask_count = np.zeros(pred_shape)
-    kernel=gkern(CFG.size,1)
-    kernel=kernel/kernel.max()
+
+def predict_fn(test_loader, model, device, test_xyxys, pred_shape):
+    mask_prediction = np.zeros(pred_shape)
+    mask_count = np.ones(pred_shape)
+    kernel = gkern(CFG.size,1)
+    kernel = kernel / kernel.max()
     model.eval()
 
     for step, (images,xys) in tqdm(enumerate(test_loader),total=len(test_loader)):
@@ -306,19 +326,33 @@ def predict_fn(test_loader, model, device, test_xyxys,pred_shape):
                 y_preds = model(images)
         y_preds = torch.sigmoid(y_preds).to('cpu')
         for i, (x1, y1, x2, y2) in enumerate(xys):
-            mask_pred[y1:y2, x1:x2] += np.multiply(F.interpolate(y_preds[i].unsqueeze(0).float(),scale_factor=16,mode='bilinear').squeeze(0).squeeze(0).numpy(),kernel)
+            mask_prediction[y1:y2, x1:x2] += np.multiply(F.interpolate(y_preds[i].unsqueeze(0).float(),scale_factor=16,mode='bilinear').squeeze(0).squeeze(0).numpy(),kernel)
             mask_count[y1:y2, x1:x2] += np.ones((CFG.size, CFG.size))
-    
-    mask_pred /= mask_count
-    return mask_pred
-import gc
+
+    mask_prediction /= mask_count
+    return mask_prediction
+
 
 if __name__ == "__main__":
-    now = datetime.now()
-    current_time = now.strftime("%Y%m%d%H%M%S")
-
+    print()
     print("Initialising...")
-    model=RegressionPLModel.load_from_checkpoint(args.model_path,strict=False)
+
+    # Initialise config variables
+    cfg_init(CFG)
+
+    # CUDA checks
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print(f"Device type: {device}")
+        print(f"Number of CUDA devices: {torch.cuda.device_count()}")
+        #torch.print_cuda_info()
+    else:
+        device = torch.device('cpu')
+        print(f"Device type: {device}")
+        print("WARNING: CUDA not available.")
+    print()
+
+    model = RegressionPLModel.load_from_checkpoint(args.model_path, strict=False)
     model.cuda()
     model.eval()
     wandb.init(
@@ -327,42 +361,51 @@ if __name__ == "__main__":
     )
 
     for fragment_id in args.segment_id:
-        if os.path.exists(f"{args.segment_path}/{fragment_id}/layers/32.{args.format}"):
-            preds=[]
+        if glob.glob(f"{args.segment_path}/{fragment_id}/layers/*.tif"):
+            predictions = []
             for r in [0]:
                 for i in [args.start]:
                     start_f = i
-                    end_f = start_f + (CFG.num_layers)
-                    test_loader,test_xyxz,test_shape,fragment_mask=get_img_splits(fragment_id,start_f,end_f,r)
-                    mask_pred= predict_fn(test_loader, model, device, test_xyxz,test_shape)
-                    mask_pred=np.clip(np.nan_to_num(mask_pred),a_min=0,a_max=1)
-                    mask_pred/=mask_pred.max()
+                    end_f = start_f + CFG.num_layers
+                    test_loader, test_xyxz, test_shape, fragment_mask = get_img_splits(fragment_id, start_f, end_f, r)
+                    mask_prediction = predict_fn(test_loader, model, device, test_xyxz,test_shape)
+                    mask_prediction = np.clip(np.nan_to_num(mask_prediction), a_min=0, a_max=1)
+                    mask_prediction /= mask_prediction.max()
 
-                    preds.append(mask_pred)
+                    predictions.append(mask_prediction)
 
-            img=wandb.Image(
-            preds[0], 
-            caption=f"{fragment_id}"
+            img = wandb.Image(
+                predictions[0],
+                caption=f"{fragment_id}"
             )
-            wandb.log({'predictions':img})
+            wandb.log({'predictions': img})
             gc.collect()
 
             if len(args.out_path) > 0:
                 # CV2 image
-                image_cv = (mask_pred * 255).astype(np.uint8)
+                image_cv = (mask_prediction * 255).astype(np.uint8)
                 try:
-                    os.makedirs(f"{args.out_path}/{fragment_id}",exist_ok=True)
+                    os.makedirs(f"{args.out_path}/{fragment_id}", exist_ok=True)
                 except:
                     pass
+                print()
                 print("Saving predictions...")
-                cv2.imwrite(os.path.join(f"{args.out_path}/{fragment_id}",
-                                         f"{fragment_id}_prediction_n{args.num_layers}s{start_f}e{end_f-1}_{current_time}.png"),
-                                         image_cv)
+                # Generate a timestamp for this inference
+                now = datetime.now()
+                current_time = now.strftime("%Y%m%d%H%M%S")
+                cv2.imwrite(os.path.join(
+                    f"{args.out_path}/{fragment_id}",
+                    f"{fragment_id}_prediction_n{args.num_layers}s{start_f}e{end_f-1}_{current_time}.png"),
+                    image_cv
+                )
                 print("Done.")
                 #output_path = f"{args.out_path}/{fragment_id}"
                 #output_path = os.path.realpath(output_path)
-                #os.startfile(output_path)
-    del mask_pred,test_loader,model
+                #os.startfile
+        else:
+            print("ERROR: Could not find a valid layer .tif file to run inference on.")
+    del mask_prediction, test_loader, model
     torch.cuda.empty_cache()
     gc.collect()
     wandb.finish()
+    print()
